5407d2596bd7be35480885f90acff91bea77285e
Avi Vajpeyi <avi.vajpeyi@ligo.org>
2019-05-01 16:22:10 +1000

    added video file check

--- images_to_video.py
+++ images_to_video.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import cv2
+from hurry.filesize import size
 
 
 def main():
@@ -31,10 +32,12 @@ def get_image_filepaths(images_path):
 
 def make_vid(images_path, video_name):
     image_filepaths = get_image_filepaths(images_path)
-
+    print("Number of images: " + str(len(image_filepaths)))
     if image_filepaths:
         img = cv2.imread(image_filepaths[0], 0)
         new_dim = rescaled_dim(img, scale_percent=30)
+        print("Orig size: (" + img.shape[1] + ", " + img.shape[0] + ")")
+        print("New size: (" + img.shape[1] + ", " + img.shape[0] + ")")
 
         video = cv2.VideoWriter(
             filename=video_name, fourcc=0, fps=10, frameSize=new_dim
@@ -44,4 +47,7 @@ def make_vid(images_path, video_name):
             image = cv2.imread(image_filepath, 0)
             rescaled_image = cv2.resize(image, new_dim, interpolation=cv2.INTER_AREA)
             video.write(rescaled_image)
+
+        mem_size = size(os.path.getsize(video_name))
+        print("Video filesize: " + mem_size)
         video.release()
