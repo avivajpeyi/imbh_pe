8b7092d775ad8e0abf3f2cf43f431355d65e5e4d
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2019-10-11 13:31:17 +1100

    add evid labels

--- imbh/run_duty_cycle_hyperpe.py
+++ imbh/run_duty_cycle_hyperpe.py
@@ -8,9 +8,21 @@ from hyper_pe.duty_cycle import sample_duty_cycle_likelihood
 
 logging.basicConfig(level=logging.DEBUG)
 
+INJECTION_NUMBER = "InjNum"
+LOG_BF = "log_bayes_factor"
+LOG_EVIDENCE = "log_evidence"
+LOG_NOISE_EVIDENCE = "log_noise_evidence"
+LOG_GLITCH_H_EVIDENCE = "log_glitchH_evidence"
+LOG_GLITCH_L_EVIDENCE = "log_glitchL_evidence"
+
 
 def start_duty_cycle_sampling(evid_csv_path):
-    evid_df = pd.read_csv(evid_csv_path)
+    csv_df = pd.read_csv(evid_csv_path)
+
+    evid_df = csv_df[
+        [LOG_EVIDENCE, LOG_NOISE_EVIDENCE, LOG_GLITCH_H_EVIDENCE, LOG_GLITCH_L_EVIDENCE]
+    ]
+
     sample_duty_cycle_likelihood(evid_df, os.path.dirname(evid_csv_path))
 
 
