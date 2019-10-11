4ee3022a2594d9677b957a896ae4b7da72c19635
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2019-10-11 11:25:33 +1100

    hack for non-injections

--- imbh/pe_result/regex.py
+++ imbh/pe_result/regex.py
@@ -1,9 +1,15 @@
-RESULT_FILE = "(.*)injection(.*?)_result.json$"
-
-H1L1_RESULT_FILE = "(.*)_H1L1_(.*)injection(.*?)_result.json$"
-H1_RESULT_FILE = "(.*)_H1_(.*)injection(.*?)_result.json$"
-L1_RESULT_FILE = "(.*)_L1_(.*)injection(.*?)_result.json$"
+# RESULT_FILE = "(.*)injection(.*?)_result.json$"
+#
+# H1L1_RESULT_FILE = "(.*)_H1L1_(.*)injection(.*?)_result.json$"
+# H1_RESULT_FILE = "(.*)_H1_(.*)injection(.*?)_result.json$"
+# L1_RESULT_FILE = "(.*)_L1_(.*)injection(.*?)_result.json$"
 INJECTION_NUM = ".*injection(\\d+|$).*_result.json"
+
+L1_RESULT_FILE = "(.*)_L1_(.*?)_result.json$"
+H1_RESULT_FILE = "(.*)_H1_(.*?)_result.json$"
+H1L1_RESULT_FILE = "(.*)_H1L1_(.*?)_result.json$"
+RESULT_FILE = "(.*)_result.json$"
+
 
 RESULT_FILE_GIVEN_I_NUMBER = (
     "(.*)_{detector_string}_(.*)injection{injection_number}_result.json$"
